5ed24794a01c7f34ace2c79e61ee9fae20ba2f83
Lea <leandrotosunian@hotmail.com>
2025-04-03 12:07:14 -0300

    First project upload

---
+++ src/components/Footer.vue
@@ -0,0 +1,131 @@
+<template>
+    <div>
+        <footer class="main-footer">
+            
+            <!-- Main block - menus, subscribe form-->
+            <div class="text-muted borde" style="background-color: #000000; padding-top: 2.4rem;"> 
+              <div class="container">
+                <div class="row">
+
+                  <div class="mb-5 col-lg-4 mb-lg-0">
+                    <div class="mb-3 text-lg fw-bold text-uppercase text-dark">
+                      <img src="@/assets/footerlogo.png" style="width:240px;" alt="eMart brand logo">
+                      <!-- <img src="@/assets/new-deco.png" class="logoImg" style="width:47px!important;" alt=""> -->
+                    </div>
+                    
+                  </div>
+                  
+                  <div class="mb-3 col-lg-2 col-md-6 mb-lg-0">
+                    <h6 class="mb-3 text-white text-uppercase">Shop</h6>
+                    <ul class="list-unstyled">
+                      <li> <router-link to="/products?category=STICKERS#!" class="text-white orangeHover" href="#">Stickers</router-link></li>
+                      <li> <router-link to="/products?category=PARCHES#!" class="text-white orangeHover" href="#">Parches Textiles</router-link></li>
+                    </ul>
+                  </div>
+                  
+                  <div class="mb-3 col-lg-2 col-md-6 mb-lg-0">
+                    <h6 class="mb-3 text-white text-uppercase">Clientes</h6>
+                    <ul class="list-unstyled">
+                      <li> <router-link to="/login" class="text-white orangeHover">Iniciar Sesión</router-link></li>
+                      <li> <router-link to="/login" class="text-white orangeHover">Quiero registrarme</router-link> </li>
+                      <li><span v-b-modal.modal-lg-buy variant="primary" class="orangeHover" style="border:none; margin-top:0px; margin-left:0!important;" >
+                        Cómo comprar
+                      </span></li>
+                      <li> <a class="text-white orangeHover" href="https://api.whatsapp.com/send?phone=5492235892260" target="_blank" >Atención al cliente</a></li>
+                    </ul>
+
+
+                  <b-modal id="modal-lg-buy" size="lg" title="Cómo comprar" >
+                    <div style="display:flex;flex-direction:column;padding:0 30px; gap:10px">
+                      <span style="font-weight:600; font-size: 18px;">
+                          - Registrate e iniciá sesión
+                          <p style="font-weight:400; font-size: 17px;">Para comprar debes ser un usuario registrado. Creá tu cuenta haciendo <router-link to="/login">click acá</router-link> y luego iniciá sesión.</p>
+                      </span>
+                      <span style="font-weight:600; font-size: 18px;">
+                          - Agregá un domicilio de entrega
+                          <p style="font-weight:400; font-size: 17px;">En las configuraciones de tu cuenta, ingresá hasta dos direcciones de envío para seleccionar al hacer tus compras.</p>
+                      </span>
+                      <span style="font-weight:600; font-size: 18px; margin-bottom: 16px;">
+                        - Agregá productos a tu carrito
+                      </span>
+                      <span style="font-weight:600; font-size: 18px;">
+                        - Realizá el pedido
+                        <p style="font-weight:400; font-size: 17px;">Elegí cómo querés que te llegue tu pedido, seleccioná tu dirección de envío que agregaste anteriormente y aboná tu compra de manera segura mediante MercadoPago.</p>
+                      </span>
+                      <span style="font-weight:600; font-size: 18px;">
+                        - Cómo sigo mi pedido?
+                        <p style="font-weight:400; font-size: 17px;">Cuando despachemos tu pedido, te enviaremos un código de seguimiento para que sigas el estado de tu compra. Éste lo podrás ver ingresando a Mis pedidos, donde en el detalle del pedido se mostrará luego de que te lo enviemos. </p>
+                      </span>
+                    </div>
+                    <template #modal-footer>
+                      <div class="">
+                        
+     
+                      </div>
+                    </template>
+                  </b-modal>
+                  </div>
+                  
+                  <div class="col-lg-2">
+                    <h6 class="mb-3 text-white text-uppercase">Contactanos</h6>
+                    
+                    <div style="display:flex; flex-direction:column; gap:8px">
+                      <a class="text-muted text-decoration-none" href="#" target="_blank" title="Instagram">
+                        <img src="/assets/instagram.png" alt="" style="width:21px; margin-right: 10px;">
+                        <span class="text-white orangeHover">unc7600</span>
+                      </a>
+                      <a class="text-muted text-decoration-none" href="https://api.whatsapp.com/send?phone=5492235892260" target="_blank" title="Whatsapp">
+                        <img src="/assets/whatsapp.png" alt="" style="width:23px; margin-right: 8px;">
+                        <span class="text-white orangeHover">+54 9 223 5 892260</span>
+                      </a>
+                    </div>
+
+                  </div>
+
+                </div>
+              </div>
+            </div>
+            <!-- Copyright section of the footer-->
+            <div class="text-white fw-light" style="background-color: #000000; padding-bottom:1.2rem; padding-top: 2rem;">
+              <div class="container">
+                <div class="row align-items-center">
+                  <div class="text-center col-md-6 text-md-start">
+                    <p class="mb-md-0">Unique Nifty Crafts &copy; Todos los derechos reservados. Tienda desarrollada por <a style="color:rgb(254, 75, 0)" target='_blank' href="https://tosunian.dev">tosunian.dev</a> </p>
+                  </div>
+                  <div class="col-md-6">
+                    <ul class="mt-2 mb-0 text-center list-inline mt-md-0 text-md-end">
+                      <li class="list-inline-item">
+                        <img class="w-2rem" src="https://d19m59y37dris4.cloudfront.net/sell/2-0/img/visa.svg" alt="...">
+                      </li>
+                      <li class="list-inline-item">
+                        <img class="w-2rem" src="https://d19m59y37dris4.cloudfront.net/sell/2-0/img/mastercard.svg" alt="...">
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </footer>
+    </div>
+</template>
+
+<style>
+  .orangeHover {
+    color: white;
+    transition: .3s all ease-in-out;
+  }
+  .orangeHover:hover {
+    color: rgb(254, 75, 0) !important;
+    transition: .3s all ease-in-out;
+    text-decoration: none;
+  }
+</style>
+
+<script>
+
+export default {
+  name: 'Footer',
+}
+</script>
+  
+  
---
+++ src/views/Home.vue
@@ -0,0 +1,265 @@
+<template>
+  <div>
+    <section>
+      <div class="container-fluid sectionFix bannersCont" >
+        <div class="row bannersCont">
+          <!-- BANNER STICKERS -->
+          <router-link to="/products?category=STICKERS#!" class="col-lg-6 dark-overlay split-screen-column align-items-start" style="background-image: url(../assets/banner1low.png); background-color: #d2cecb; text-decoration: none"> 
+            <div class="mt-7 overlay-content px-lg-5 bannerTitle">
+              <h1 class="text-white display-4 fw-bold split-screen-heading">STICKERS</h1>
+            </div>
+          </router-link>
+          <!-- BANNER PARCHES -->
+          <router-link  to="/products?category=POSTERS#!" class="col-lg-6 dark-overlay split-screen-column align-items-start" style="background-image: url(../assets/banner2low.png); background-color: #e5b6b4; text-decoration: none"> 
+          <!-- <div class="col-lg-6 split-screen-column align-items-center align-items-lg-end" style="background-image: url(../assets/backk2.jpg); background-color: #e5b6b4;">  -->
+            <div class=" overlay-content px-lg-5 mb-lg-5 bannerTitle" >
+              <h1 class=" display-4 fw-bold split-screen-heading" style="color: white;">POSTERS</h1>
+            </div>
+          </router-link>
+
+        </div>
+      </div>
+    </section>
+
+    <div class="sectionTitle">
+      <span>NUEVAS CAPSULAS</span>
+    </div>
+
+    <section class="pb-2 " style="margin: 70px 0">
+      <div class="container">
+        <!-- <div class="row">
+          <div class="mx-auto text-center col-xl-8">
+            <h2 class="text-uppercase">NUEVOS PRODUCTOS</h2>
+            <p class="lead text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div> -->
+
+        <div class="row">
+          <!-- product-->
+          <div class="col-lg-3 col-md-4 col-6" v-for="item in fourProducts" >
+            <router-link :to="/product/+item.slug">
+              <div >
+                <div class="product-image" style="border: none !important;">
+                  <!-- <div class="ribbon ribbon-info" v-if="item.discount">En oferta</div> -->
+                  <img class="img-fluid productBorderRadius" :src="$url+'/getGallery/'+item.image" alt="product"/>
+                  <div class="product-hover-overlay"><a class="product-hover-overlay-link" href=""></a>
+                    <div class="product-hover-overlay-buttons">
+                      <a class="btn btn-dark btn-buy" style="background-color: black; border-radius: 10px; padding: 12px 10px; height: fit-content; line-height: 20px;" href=""><span class="btn-buy-label ms-2">{{item.name}}</span></a>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+            </router-link>
+          </div>
+          <!-- /product-->
+        </div>
+
+      </div>
+    </section>
+
+    <div class="sectionTitle">
+      <span>LO MAS VENDIDO</span>
+    </div>
+    
+    <section class="py-4 bg-white" style="margin: 70px 0">
+      <div class="container">
+        <!-- <div class="row">
+          <div class="mx-auto mb-5 text-center col-xl-8">
+            <h2>LO MAS VENDIDO</h2>
+            <p class="lead text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div> -->
+
+        <!-- <div class="row">
+          <div class="mb-4 col-md-6 d-flex align-items-md-stretch mb-md-0">
+            <div class="py-6 text-center text-white d-flex align-items-center dark-overlay w-100 py-md-0 model1img" style="">
+             <a class="tile-link" href="category.html"> </a>
+              <div class="card-img-overlay w-100 overlay-content">
+                <h2 class="mb-0 display-2 fw-bold text-uppercase">UPTEMPO</h2>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="mb-4 text-center text-white border-0 card dark-overlay">
+              <img class="card-img" src="../assets/model3.jpeg" alt="Ladies"><a class="tile-link" href="category.html"> </a>
+              <div class="card-img-overlay d-flex align-items-center">
+                <div class="text-center w-100 overlay-content">
+                  <h2 class="mb-0 text-center display-4 fw-bold text-uppercase">RETRO 4</h2>
+                </div>
+              </div>
+            </div>
+            <div class="text-center text-white border-0 card dark-overlay">
+              <img class="card-img" src="../assets/model2.jpg" alt="Accessories"><a class="tile-link" href="category.html"> </a>
+              <div class="card-img-overlay d-flex align-items-center">
+                <div class="text-center w-100 overlay-content">
+                  <h2 class="mb-0 text-center display-4 fw-bold text-uppercase">AIR MAX</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div> -->
+
+        <div class="row">
+          <!-- product-->
+          <div class="col-lg-3 col-md-4 col-6" v-for="item in mostWantedProducts" >
+            <router-link :to="/product/+item.slug">
+              <div class="">
+                <div class="product-image" style="border: none !important;">
+                  <div class="ribbon ribbon-info" v-if="item.discount">En oferta</div>
+                  <img class="img-fluid productBorderRadius" :src="$url+'/getImage/'+item.image" alt="product"/>
+                  <div class="product-hover-overlay"><a class="product-hover-overlay-link" href="detail.html"></a>
+                    <div class="product-hover-overlay-buttons" >
+                      <a class="btn btn-dark btn-buy" style="background-color: black; border-radius: 10px;" href=""><span class="btn-buy-label ms-2">Ver más</span></a>
+                    </div>
+                  </div>
+                </div>
+                <div class="py-2">
+                  <p class="mb-1 text-sm text-muted">{{item.category}}</p>
+                  <h3 class="mb-1 h6" style="text-transform:uppercase;">
+                    <a class="text-dark" href="detail.html">{{item.name}}</a>
+                  </h3>
+                  <span class="text-muted">{{ priceConverter(item.price) }} </span>
+                </div>
+              </div>
+            </router-link>
+          </div>
+          <!-- /product-->
+        </div>
+
+      </div>
+    </section>
+
+  </div>
+</template>
+
+<style>
+  .model1img{
+    background: center center url('../assets/model1.jpg') no-repeat; background-size: cover;
+  }
+  .product-image{
+    display: block;
+    position: relative;
+    overflow: hidden;
+    height: 320px;
+  }
+  .sectionFix{
+    margin-top: 0px;
+  }
+
+  .sectionTitle {
+    width: 100%; 
+    padding-bottom:8px; 
+    height: 100px; 
+    background-color:black; 
+    display:flex; 
+    justify-content: center; 
+    align-items: center;
+  }
+
+  .sectionTitle span {
+    color:rgb(254, 75, 0); 
+    font-size:40px; 
+    font-weight:600;
+  }
+  
+
+  .bannerTitle {
+    padding-top:170px ;
+    margin: 0 auto !important;
+
+  }
+
+  @media (max-width:991px){
+    .sectionFix{
+      margin-top: 91.38px;
+    }
+    .bannersCont {
+      height: calc(100vh - 91.38px);
+    }
+    .sectionTitle {
+      padding-bottom:8px; 
+      height: 70px; 
+      padding-bottom: 4px;
+    }
+    .sectionTitle span {
+      font-size:24px; 
+      font-weight:600;
+    }
+    .bannerTitle {
+      padding-top:35px ;
+    }
+  }
+</style>
+
+<script>
+// @ is an alias to /src
+import axios from 'axios'
+import currencyFormatter from 'currency-formatter'
+import { initCarousel } from '../../public/assets/js/theme.d7b4a888'
+import carousel from 'vue-owl-carousel'
+
+export default {
+  name: 'Home',
+  components: {
+    carousel
+  },
+  data(){
+    return {
+      fourProducts: [],
+      mostWantedProducts: [],
+      freeShipping: ''
+    }
+  },
+  mounted () {
+  window.scrollTo(0, 0)
+  },
+  methods: {
+    getFourProducts(){
+      var loader = this.$loading.show({
+        container: this.fullPage ? null : this.$refs.formContainer,
+        canCancel: false,
+        color: "orange",
+        opacity: 1
+      });
+      axios.get(this.$url+'/public/getLatestProducts', {
+        headers: {
+          "Content-Type": 'application/json',
+        }
+        }).then((response) => {
+          const {data} = response
+          this.fourProducts = data
+          loader.hide()
+        }).catch( error => {
+          this.msm_error = error.response.data.msg
+          loader.hide()
+        })
+        
+    },
+    getMostWantedProducts(){
+      axios.get(this.$url+'/public/getMostWantedProducts', {
+            headers: {
+              "Content-Type": 'application/json',
+            }
+            }).then((response) => {
+              const {data} = response
+              this.mostWantedProducts = data
+              log
+            }).catch( error => {
+              console.log(error.response.data.msg)
+              this.msm_error = error.response.data.msg
+               
+            })
+    },
+    priceConverter(price){
+        return currencyFormatter.format(price, { code: 'ARS' });
+
+    },
+  },
+  beforeMount(){
+    this.getFourProducts()
+    this.getMostWantedProducts()
+    initCarousel.init()
+  }
+}
+</script>
